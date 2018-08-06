18baa3b36763f8e1200af6eba64e01d457f2677f
szcgy <13509664878@139.com>
2018-08-06 16:52:06 +0800

    搞掂
    Signed-off-by: szcgy <13509664878@139.com>

--- MySQLTools/MySqlTools.py
+++ MySQLTools/MySqlTools.py
@@ -1,7 +1,8 @@
 from mysql import connector
 import os
+import threading
 
-class MySqlTools:
+class Connection:
     """
     `host` 数据库地址
 
@@ -19,6 +20,8 @@ class MySqlTools:
     user='root'
     database=''
     cnt = None
+    lock = threading.Lock()
+
     def __init__(self,host='localhost',port=3306,user='root',password='*',database=''):
         self.host=host
         self.port=port
@@ -53,42 +56,45 @@ class MySqlTools:
             return False
         if(self.__connect()):
             try:
-                cur = self.cnt.cursor()
                 cmd = ("INSERT INTO `%s` ("%tableName)
                 values = "VALUES ("
                 for clumnName in data.keys():
                     cmd += clumnName+','
                     values +="%({0})s,".format(clumnName)
                 cmd = cmd[0:-1]+") "+values[0:-1]+")"
+                self.lock.acquire()
+                cur = self.cnt.cursor()
                 cur.execute(cmd,data)
                 self.cnt.commit()
                 cur.close()
-                self.cnt.close()
+                self.lock.release()
                 return True
             except Exception as e:
                 print(e)
+                self.lock.release()
                 return False
         else:
             return False
 
     def delete(self,tableName='',condition=''):
-        """
-        删除行
-
-        `tableName` 表名
-
-        `condition` SQL语句中WHERE部分 例如 "WHERE \`id\` = '1'"
-
-        返回 True 或者 False
+        """\
+        删除行\
+\
+        `tableName` 表名\
+\
+        `condition` SQL语句中WHERE部分 例如 "WHERE \`id\` = '1'"\
+\
+        返回 True 或者 False\
         """
         if(self.__connect()):
             try:
-                cur = self.cnt.cursor()
                 cmd = 'DELETE FROM {0} {1}'.format(tableName,condition)
+                self.lock.acquire()
+                cur = self.cnt.cursor()
                 cur.execute(cmd)
                 self.cnt.commit()
                 cur.close()
-                self.cnt.close()
+                self.lock.release()
                 return True
             except Exception as e:
                 print(e)
@@ -110,16 +116,17 @@ class MySqlTools:
         """
         if(self.__connect()):
             try:
-                cur = self.cnt.cursor()
                 values = ""
                 for clumnName in data.keys():
                     values +="`{0}`=%({0})s,".format(clumnName)
                 values =values[0:-1]
                 cmd = 'UPDATE {0} SET {1} {2}'.format(tableName,values,condition)
+                self.lock.acquire()
+                cur = self.cnt.cursor()
                 cur.execute(cmd,data)
                 self.cnt.commit()
                 cur.close()
-                self.cnt.close()
+                self.lock.release()
                 return True
             except Exception as e:
                 print(e)
@@ -141,7 +148,7 @@ class MySqlTools:
         """
         if(self.__connect()):
             try:
-                cur = self.cnt.cursor()
+                
                 col = '*'
                 if(len(columns)>0):
                     col = ''
@@ -150,12 +157,16 @@ class MySqlTools:
                     col = col[0:-1]
                 else:
                     cmd = "SELECT `COLUMN_NAME` FROM `information_schema`.`columns` WHERE table_name='{0}' and table_schema='{1}'".format(tableName,self.cnt._database)
+                    self.lock.acquire()
+                    cur = self.cnt.cursor()
                     cur.execute(cmd)
                     for c in cur.fetchall():
                         columns += c
                     cur.close()
-                    cur = self.cnt.cursor()
+                    self.lock.release()
                 cmd = 'SELECT {0} FROM `{1}` {2}'.format(col,tableName,condition)
+                self.lock.acquire()
+                cur = self.cnt.cursor()
                 cur.execute(cmd)
                 datalist = []
                 for row in cur:
@@ -164,11 +175,12 @@ class MySqlTools:
                         di[columns[i]] = row[i]
                     datalist.append(di)
                 cur.close()
-                self.cnt.close()
+                self.lock.release()
                 return datalist
             except Exception as e:
                 print(e)
                 return []
+                self.lock.release()
         else:
             return []
 
@@ -177,26 +189,125 @@ class MySqlTools:
             os.makedirs(filePath)
         if self.__connect():
             cur = self.cnt.cursor()
-            cmd = "SELECT `table_name`,`COLUMN_NAME` FROM `information_schema`.`columns` WHERE table_schema='{0}'".format(self.cnt._database)
+            cmd = "SELECT `table_name`,`COLUMN_NAME`,`DATA_TYPE`,`COLUMN_KEY`,`EXTRA` FROM `information_schema`.`columns` WHERE table_schema='{0}'".format(self.cnt._database)
             cur.execute(cmd)
-            model = {}
+            tabelModel = {}
             for row in cur:
-                if row[0] in model:
-                    model[row[0]] += (row[1],)
+                if row[0] in tabelModel:
+                    tabelModel[row[0]] += ((row[1],row[2],row[3],row[4]),)
                 else:
-                    model[row[0]] = ()
-                    model[row[0]] += (row[1],)
-            for table in model.items():
+                    tabelModel[row[0]] = ()
+                    tabelModel[row[0]] += ((row[1],row[2],row[3],row[4]),)
+            cur.close()
+            for table in tabelModel.items():
                 f=open('%s\\%s.py'%(filePath,table[0]), 'w')
+                f.write("from MySQLTools.MySqlTools import Model\nfrom MySQLTools.MySqlTools import Connection\nimport datetime\n")
+                pri = ''
+                ai = ''
+                defalt = ''
                 for column in table[1]:
-                    f.write("{0} = '{0}'\n".format(column))
+                    f.write("{0} = '{0}'\n".format(column[0]))
+                    if column[2] == 'PRI':
+                        pri += '%s, ' % column[0]
+                    if column[3] == 'auto_increment':
+                        ai = column[0]
+                    ctype = column[1]
+                    if (ctype == 'bigint'
+                    or ctype == 'decimal'
+                    or ctype =='double'
+                    or ctype == 'enum'
+                    or ctype == 'int'
+                    or ctype == 'smallint'
+                    or ctype == 'tinyint'):
+                        defalt += '%s : 0, ' % column[0]
+                    elif (ctype == 'blob'
+                    or ctype == 'longblob'):
+                        defalt += "%s : b'', " % column[0]
+                    elif (ctype == 'char'
+                    or ctype == 'longtext'
+                    or ctype == 'mediumtext'
+                    or ctype == 'text'
+                    or ctype == 'varchar'):
+                        defalt += "%s : '', " % column[0]
+                    elif (ctype == 'datetime'
+                    or ctype == 'timestamp'):
+                        defalt += "%s : datetime.datetime(datetime.MINYEAR,1,1,0,0,1), " % column[0]
+                    else:
+                        print('Undefind data type "%s"' % column[0])
+                        defalt += "%s : None, " % column[0]
+                defalt = defalt[0:-2]
+                pri = pri[0:-1]
+                f.write('class %s(Model):\n' % table[0])
+                f.write('\tdef __new__(cls,connection,args = None):\n')
+                f.write("\t\treturn super().__new__(cls,connection,((%s),'%s','%s'))\n\n" % (pri,table[0],ai))
+                f.write("\tdef __init__(self,connection,content=None):\n")
+                f.write("\t\tif content is None:\n")
+                f.write("\t\t\tsuper().__init__({%s})\n" % defalt)
+                f.write("\t\telse:\n")
+                f.write("\t\t\tsuper().__init__(content)\n")
                 f.close()
+
+class Model(dict):
+    tableName = ''
+    key = ()
+    aicolumn = ''
+    conn = None
+    
+    def __init__(self,content):
+        for each in content.items():
+            self[each[0]] = each[1]
+
+    def __new__(cls,connection,args):
+        cls.conn = connection
+        cls.key = args[0]
+        cls.tableName = args[1]
+        cls.aicolumn = args[2]
+        return dict.__new__(cls)
+
+    @classmethod
+    def findAll(cls,condition):
+        data = cls.conn.select(cls.tableName,condition = condition)
+        result = []
+        for each in data:
+            result.append(cls(cls.conn,each))
+        return result
+
+    @classmethod
+    def find(cls,pk):
+        conditionStr = "WHERE "
+        keyLen = len(cls.key)
+        if len(pk)!=keyLen:
+            print("pk length dosen't match!!!")
+            return None
+        conditionStr += "`{0}`='{1}'".format(cls.key[0],pk[0])
+        if keyLen>1:
+            for i in range(len(cls.key)-1):
+                conditionStr += " AND `{0}`='{1}'".format(cls.key[i+1],pk[i+1])
+        data = cls.conn.select(cls.tableName,condition = conditionStr)
+        if len(data)>0:
+            return cls(cls.conn,data[0])
+        return None
+    
+    def save(self,ai = True):
+        if ai:
+            if len(self.aicolumn)>0:
+                del self[self.aicolumn]
+        self.conn.insert(self.tableName,self)
+    
+    def update(self):
+        conditionStr = "WHERE `{0}`='{1}'".format(self.key[0],self.pop(self.key[0]))
+        keyLen = len(self.key)
+        if keyLen>1:
+            for i in range(len(self.key)-1):
+                conditionStr += " AND `{0}`='{1}'".format(self.key[i+1],self.pop(self.key[i+1]))
+        self.conn.update(self.tableName,self,conditionStr)
+
+
 
 
 def main():
-    connection = MySqlTools(input("Host Name:"),int(input("Port Number:")),input("User Name:"),input("Password:"),input("Database Name:"))
+    connection = Connection(input("Host Name:"),int(input("Port Number:")),input("User Name:"),input("Password:"),input("Database Name:"))
     connection.buildModel(input("Path To Build Model:"))
-    input("Input Anything To Close:")
 
 if __name__ == '__main__':
     main()
